148d3a5127a3a50cd75df00759ac94eec998eaa4
Poojachakor <chakorpooja07@gmail.com>
2023-03-26 17:15:51 +0530

    Add: UC3 file

---
+++ MoodAnalyserProject/MoodAnalysisException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProject
+{
+    public class MoodAnalysisException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE,
+            EMPTY_MESSAGE
+        }
+        public readonly ExceptionType type;
+        public MoodAnalysisException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
+
