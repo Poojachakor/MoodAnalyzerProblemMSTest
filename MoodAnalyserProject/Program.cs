ac4065bdc78e0f68c94aa6edd2a83effc258c897
Poojachakor <chakorpooja07@gmail.com>
2023-03-26 16:43:25 +0530

    Add: MoodAnalyserProjectfile

---
+++ MoodAnalyserProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyserProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
